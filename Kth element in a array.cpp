dbb3bb725083ccab8200a2f1e24eb4781eeb31c6
Ojaswini <63453938+OjaswiniN@users.noreply.github.com>
2022-03-01 17:49:50 +0530

    Update Kth element in a array.cpp

--- Kth element in a array.cpp
+++ Kth element in a array.cpp
@@ -1,9 +1,5 @@
 // Simple C++ program to find k'th smallest element
-#include <algorithm>
-#include <iostream>
-using namespace std;
 
-// Function to return k'th smallest element in a given array
 int kthSmallest(int arr[], int n, int k)
 {
 	// Sort the given array
@@ -13,12 +9,4 @@ int kthSmallest(int arr[], int n, int k)
 	return arr[k - 1];
 }
 
-// Driver program to test above methods
-int main()
-{
-	int arr[] = { 12, 3, 5, 7, 19 };
-	int n = sizeof(arr) / sizeof(arr[0]), k = 2;
-	cout << "K'th largest element is " << kthLargest(arr, n, k);
-	return 0;
-}
 
